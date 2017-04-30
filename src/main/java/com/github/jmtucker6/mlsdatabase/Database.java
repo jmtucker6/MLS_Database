aa411a1d5dd706c0f8017c72a1521069fdb1d17b
Jacob Tucker <jmtucker6@crimson.ua.edu>
2017-04-30 16:50:26 -0500

    Fixes ordering ambiguity errors

--- src/main/java/com/github/jmtucker6/mlsdatabase/Database.java
+++ src/main/java/com/github/jmtucker6/mlsdatabase/Database.java
@@ -28,18 +28,7 @@ public class Database {
 	 */
 	public Relation processQuery(Query query) {
 		List<String> tableNames = query.getTableNames();
-		Relation relation = tables.get(tableNames.get(0));
-		String joinCondition;
-		if (tableNames.size() > 1) {
-			for (int i = 1; i < tableNames.size(); i++) {
-				if(query.getJoinConditions().size() >= i) {
-					joinCondition = query.getJoinConditions().get(i-1);
-				} else {
-					joinCondition = null;
-				}
-				relation = relation.join(tables.get(tableNames.get(i)), joinCondition);
-			}
-		}
+		Relation relation = executeJoins(tableNames, query.getJoinConditions());
 		try {
 			relation.applyConditions(query.getSelectConditions(), query.getClassificationLevel());
 		} catch (Exception e) {
@@ -50,6 +39,48 @@ public class Database {
 		relation = relation.selectColumns(query.getColumnNames());
 		return relation;
 	}
+	
+	/**
+	 * handles the join stage of query execution
+	 * @param tableNames name of tables asked for by query
+	 * @param joinConditions join conditions dictated by query
+	 * @return result relation of joins
+	 */
+	private Relation executeJoins(List<String> tableNames, List<String> joinConditions) {
+		String[] tokens;
+		Relation leftRelation = null, rightRelation = null, resultRelation;
+		for (String condition : joinConditions) {
+			tokens = condition.split("=");
+			for (String tableName : tableNames) {
+				if (tables.get(tableName).getColumnNames().contains(tokens[0])) {
+					leftRelation = tables.get(tableName);
+					break;
+				}
+			}
+			for (String tableName : tableNames) {
+				if (tables.get(tableName).getColumnNames().contains(tokens[1])) {
+					rightRelation = tables.get(tableName);
+					break;
+				}
+			}
+			tableNames.remove(leftRelation.getName());
+			tableNames.remove(rightRelation.getName());
+			resultRelation = leftRelation.join(rightRelation, condition);
+			tables.put(resultRelation.getName(), resultRelation);
+			tableNames.add(resultRelation.getName());
+		}
+		while (tableNames.size() > 1) {
+			leftRelation = tables.get(tableNames.get(0));
+			rightRelation = tables.get(tableNames.get(1));
+			tableNames.remove(leftRelation.getName());
+			tableNames.remove(rightRelation.getName());
+			resultRelation = leftRelation.join(rightRelation, null);
+			tables.put(resultRelation.getName(), resultRelation);
+			tableNames.add(resultRelation.getName());
+		}
+		return tables.get(tableNames.get(0));
+	}
+	
 	
 
 }
